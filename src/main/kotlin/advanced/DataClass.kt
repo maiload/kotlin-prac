b69203496af6eb173a3150f57d8278d06e6831a3
JJ Lee <maiload77@gmail.com>
2024-05-30 22:01:35 +0900

    Data class 주석 수정

--- src/main/kotlin/advanced/DataClass.kt
+++ src/main/kotlin/advanced/DataClass.kt
@@ -2,6 +2,7 @@ package advanced
 
 // 데이터 클래스 사용 시 컴파일러가 자동으로 equals, hashcode, toString, componentN, copy 와 같은 함수를 자동으로 생성
 // kotlin bytecode 기능으로 decompile 하여 자바 코드로 확인 가능
+// 데이터 클래스의 프로퍼티를 var 로 설정할 경우 추후 예기치 않은 오류가 발생할 수 있어 불변객체로 사용하는 것을 권장 -> copy 사용
 
 data class Person(val name: String, val age: Int)
 
@@ -16,7 +17,6 @@ fun main() {
 
     println(person1.toString())
 
-    // var 로 프로퍼티를 설정할 경우 추후 예기치 않은 오류가 발생할 수 있어 불변객체로 사용하는 것을 권장 -> copy 사용
 //    person1.name = "strange"
 //    println(set.contains(person1))
 
